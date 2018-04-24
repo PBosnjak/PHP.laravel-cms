69fffe6892ae6eda5d1692d4faf9ee6043fc16c8
PBosnjak <petar.bosnjak@yahoo.com>
2018-04-24 13:58:13 +0200

    additional seeds

--- database/seeds/TaskTableSeeder.php
+++ database/seeds/TaskTableSeeder.php
@@ -20,5 +20,45 @@ class TaskTableSeeder extends Seeder
         $task->college_type = "diplomski";
         $task->save();
         $task->users()->attach(User::where('name', 'Miro')->first());
+
+        $task1 = new Task();
+        $task1->name = "ASP.NET aplikacija za diplomske radove";
+        $task1->name_en = "ASP.NET application for master thesis ";
+        $task1->goal = "Napraviti aplikaciju za prijavu na diplomske radove";
+        $task1->college_type = "diplomski";
+        $task1->save();
+        $task1->users()->attach(User::where('name', 'Miro')->first());
+
+        $task2 = new Task();
+        $task2->name = "Usporebda različitih izvora svjetlosti";
+        $task2->name_en = "Comparation of different sources of light";
+        $task2->goal = "Usporediti efikasnost i cijenu različitih izvora svjetlosti";
+        $task2->college_type = "preddiplomski";
+        $task2->save();
+        $task2->users()->attach(User::where('name', 'Lovro')->first());
+
+        $task3 = new Task();
+        $task3->name = "Upravljanje robotskim manipulatorom";
+        $task3->name_en = "Robotic manipulator management";
+        $task3->goal = "Napraviti aplikaciju za upravljanje robotskim manipulatorom";
+        $task3->college_type = "stručni";
+        $task3->save();
+        $task3->users()->attach(User::where('name', 'Lovro')->first());
+
+        $task4 = new Task();
+        $task4->name = "STM aplikacija";
+        $task4->name_en = "STM application";
+        $task4->goal = "STM aplikacija ra rukovanje LED lampicama";
+        $task4->college_type = "stručni";
+        $task4->save();
+        $task4->users()->attach(User::where('name', 'Miro')->first());
+
+        $task5 = new Task();
+        $task5->name = "Arduino pokretni robot";
+        $task5->name_en = "Arduino mobile robot";
+        $task5->goal = "Isprogramirati mobilni robot pomoću arduina";
+        $task5->college_type = "preddiplomski";
+        $task5->save();
+        $task5->users()->attach(User::where('name', 'Lovro')->first());
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -31,11 +31,32 @@ class UserTableSeeder extends Seeder
         $professor->save();
         $professor->roles()->attach($role_professor);
 
+        $professor1 = new User();
+        $professor1->name = "Lovro";
+        $professor1->email = "lovro@mail.com";
+        $professor1->password = bcrypt('123456');
+        $professor1->save();
+        $professor1->roles()->attach($role_professor);
+
         $student = new User();
         $student->name = "Ivo";
         $student->email = "ivo@mail.com";
         $student->password = bcrypt('123456');
         $student->save();
         $student->roles()->attach($role_student);
+
+        $student1 = new User();
+        $student1->name = "David";
+        $student1->email = "david@mail.com";
+        $student1->password = bcrypt('123456');
+        $student1->save();
+        $student1->roles()->attach($role_student);
+
+        $student2 = new User();
+        $student2->name = "Lucija";
+        $student2->email = "lucija@mail.com";
+        $student2->password = bcrypt('123456');
+        $student2->save();
+        $student2->roles()->attach($role_student);
     }
 }
